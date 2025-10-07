c7a0dd8626c6b6c6ee0b54ef629087161eb32165
AnnaRiccia <ricciardelli.anna99.com>
2025-10-07 18:02:24 +0200

    nnnn

--- home_analista.py
+++ home_analista.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from filtro_df import  mostra_df_filtrato_home_admin
 from grafici import aggrid_pivot, aggrid_pivot_delta
 import pandas as pd
 import plotly.graph_objects as go
+
+
 def refreshino(key_suffix=""):
     refresh = st.button("⟳", key=f"refresh_{key_suffix}")
     if refresh:
@@ -17,6 +18,55 @@ def home_analista(df, nav, df_full):
         st.title("Area Analisi")
         sezione = st.sidebar.radio("",["PIVOT", "GRAFICI"])
 
+        if sezione == "PIVOT":
+            st.subheader("DASHBOARD")
+            aggrid_pivot_delta(df,
+            group_col="CENTRO DI COSTO",
+            sub_col="NOME SERVIZIO",
+            value_col="COSTO",
+            mese_col="MESE",
+            height=500)
+
+            st.header("PIVOT")
+
+            df = df[df["INVIATE AL PROVIDER"].notnull()]
+            mesi_italiani = [
+                "Tutti", "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
+                "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
+            ]
+            df["MESE_IT"] = df["MESE"].apply(lambda x: mesi_italiani[int(x)] if pd.notnull(x) and str(x).isdigit() and 0 < int(x) <= 12 else "")
+            mesi_italiani_ordinati = mesi_italiani[1:]
+            mesi_presenti = [m for m in mesi_italiani_ordinati if m in df["MESE_IT"].values]
+            mesi = ["Tutti"] + mesi_presenti
+            anni = df["ANNO"].dropna().unique().tolist()
+            anni.sort()
+            anni.insert(0, "Tutti")
+            col1, col2 = st.columns(2)
+            with col1:
+                anno_sel = st.segmented_control("Filtra per anno", anni)
+                mese_sel = st.selectbox("Filtra per mese", mesi)
+
+            st.divider()
+            df_filtrato = df
+            if mese_sel != "Tutti":
+                df_filtrato = df_filtrato[df_filtrato["MESE_IT"] == mese_sel]
+            if anno_sel != "Tutti":
+                df_filtrato = df_filtrato[df_filtrato["ANNO"] == anno_sel]
+            col1, spacer, col2 = st.columns([1, 0.1, 1])
+            with col1:
+                aggrid_pivot(df_filtrato, "GESTORE", "PORTAFOGLIO", "COSTO", value_name="Totale Costo", group_width=80,sub_width=130,value_width=130,height=500)
+            with col2:
+                aggrid_pivot(df_filtrato, "GESTORE", "NOME SERVIZIO", "COSTO", value_name="Totale Costo", group_width=80,sub_width=120,value_width=100,height=500)
+               
+            col1, spacer, col2 = st.columns([1, 0.1, 1])
+            with col1:
+                aggrid_pivot(df_filtrato, "CENTRO DI COSTO", "NOME SERVIZIO", "COSTO", value_name="Totale Costo", group_width=80,sub_width=120,value_width=100,height=500)
+            with col2:
+                aggrid_pivot(df_filtrato, "CENTRO DI COSTO", "PORTAFOGLIO", "COSTO", value_name="Totale Importo",group_width=80,sub_width=120,value_width=100,height=500)
+            
+            col1, spacer, col2 = st.columns([1, 0.1, 1])
+            with col1:
+                aggrid_pivot(df_filtrato, "PORTAFOGLIO", "GESTORE", "COSTO", value_name="Totale Importo",group_width=80,sub_width=120,value_width=100,height=500)
         if sezione == "GRAFICI":
             col1, col2 = st.columns(2)
             with col1:
@@ -100,6 +150,10 @@ def home_analista(df, nav, df_full):
 
 
 
+
+
+
+
             df_line = df[df["ANNO"].isin([2024, 2025])].copy()
             df_line["MESE"] = pd.to_numeric(df_line["MESE"], errors="coerce")
 
@@ -150,55 +204,6 @@ def home_analista(df, nav, df_full):
                 height=600
             )
             st.plotly_chart(fig, use_container_width=True)
-######################## GRAFICO GESTORI ###########################
 
 
-        if sezione == "PIVOT":
-            st.subheader("DASHBOARD")
-            aggrid_pivot_delta(df,
-            group_col="CENTRO DI COSTO",
-            sub_col="NOME SERVIZIO",
-            value_col="COSTO",
-            mese_col="MESE",
-            height=500)
 
-            st.header("PIVOT")
-
-            df = df[df["INVIATE AL PROVIDER"].notnull()]
-            mesi_italiani = [
-                "Tutti", "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno",
-                "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"
-            ]
-            df["MESE_IT"] = df["MESE"].apply(lambda x: mesi_italiani[int(x)] if pd.notnull(x) and str(x).isdigit() and 0 < int(x) <= 12 else "")
-            mesi_italiani_ordinati = mesi_italiani[1:]
-            mesi_presenti = [m for m in mesi_italiani_ordinati if m in df["MESE_IT"].values]
-            mesi = ["Tutti"] + mesi_presenti
-            anni = df["ANNO"].dropna().unique().tolist()
-            anni.sort()
-            anni.insert(0, "Tutti")
-            col1, col2 = st.columns(2)
-            with col1:
-                anno_sel = st.segmented_control("Filtra per anno", anni)
-                mese_sel = st.selectbox("Filtra per mese", mesi)
-
-            st.divider()
-            df_filtrato = df
-            if mese_sel != "Tutti":
-                df_filtrato = df_filtrato[df_filtrato["MESE_IT"] == mese_sel]
-            if anno_sel != "Tutti":
-                df_filtrato = df_filtrato[df_filtrato["ANNO"] == anno_sel]
-            col1, spacer, col2 = st.columns([1, 0.1, 1])
-            with col1:
-                aggrid_pivot(df_filtrato, "GESTORE", "PORTAFOGLIO", "COSTO", value_name="Totale Costo", group_width=80,sub_width=130,value_width=130,height=500)
-            with col2:
-                aggrid_pivot(df_filtrato, "GESTORE", "NOME SERVIZIO", "COSTO", value_name="Totale Costo", group_width=80,sub_width=120,value_width=100,height=500)
-               
-            col1, spacer, col2 = st.columns([1, 0.1, 1])
-            with col1:
-                aggrid_pivot(df_filtrato, "CENTRO DI COSTO", "NOME SERVIZIO", "COSTO", value_name="Totale Costo", group_width=80,sub_width=120,value_width=100,height=500)
-            with col2:
-                aggrid_pivot(df_filtrato, "CENTRO DI COSTO", "PORTAFOGLIO", "COSTO", value_name="Totale Importo",group_width=80,sub_width=120,value_width=100,height=500)
-            
-            col1, spacer, col2 = st.columns([1, 0.1, 1])
-            with col1:
-                aggrid_pivot(df_filtrato, "PORTAFOGLIO", "GESTORE", "COSTO", value_name="Totale Importo",group_width=80,sub_width=120,value_width=100,height=500)
