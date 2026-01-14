b9ff2c5d23328e1ebd9fbe855c46573a3c0b2906
AnnaRiccia <ricciardelli.anna99.com>
2026-01-14 12:37:37 +0100

    nnnn

--- user_pag/Le mie Diffide - Telegrammi.py
+++ user_pag/Le mie Diffide - Telegrammi.py
@@ -5,13 +5,15 @@ from nav_dt import visualizza_richieste_personali_dt
 def main(**kwargs):
 
     st.title("Diffide, Welcome Letter e Telegrammi Inviati")
-    df_dt = kwargs.get('df_dt_full')
-    navigator_dt = kwargs.get('navigator_dt')
+    st.success("------------------ IN ATTESA DI APPROVAZIONE -------------------      \n\n" \
+    "Nuova funzionalità per richiedere l'invio di Diffide, Welcome Letter e Telegrammi")
+    # df_dt = kwargs.get('df_dt_full')
+    # navigator_dt = kwargs.get('navigator_dt')
 
-    st.subheader("Dati Richieste")
-    st.info("per visualizzare tutte le richieste clicca sull'icona di aggiornamento e attendi qualche secondo.")
-    visualizza_richieste_personali_dt(navigator_dt, df_dt)
-    st.divider()
+    # st.subheader("Dati Richieste")
+    # st.info("per visualizzare tutte le richieste clicca sull'icona di aggiornamento e attendi qualche secondo.")
+    # visualizza_richieste_personali_dt(navigator_dt, df_dt)
+    # st.divider()
 
 
 if __name__ == "__main__":
