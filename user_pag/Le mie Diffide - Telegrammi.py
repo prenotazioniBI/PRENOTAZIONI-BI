df0b3b455f8d1a66ba093925e1c46b08c50e0a19
AnnaRiccia <ricciardelli.anna99.com>
2026-01-15 14:50:13 +0100

    Aggiornamento completo progetto

--- user_pag/Le mie Diffide - Telegrammi.py
+++ user_pag/Le mie Diffide - Telegrammi.py
@@ -7,13 +7,13 @@ def main(**kwargs):
     st.title("Diffide, Welcome Letter e Telegrammi Inviati")
     st.success("------------------ IN ATTESA DI APPROVAZIONE -------------------      \n\n" \
     "Nuova funzionalità per richiedere l'invio di Diffide, Welcome Letter e Telegrammi")
-    # df_dt = kwargs.get('df_dt_full')
-    # navigator_dt = kwargs.get('navigator_dt')
+    df_dt = kwargs.get('df_dt_full')
+    navigator_dt = kwargs.get('navigator_dt')
 
-    # st.subheader("Dati Richieste")
-    # st.info("per visualizzare tutte le richieste clicca sull'icona di aggiornamento e attendi qualche secondo.")
-    # visualizza_richieste_personali_dt(navigator_dt, df_dt)
-    # st.divider()
+    st.subheader("Dati Richieste")
+    st.info("per visualizzare tutte le richieste clicca sull'icona di aggiornamento e attendi qualche secondo.")
+    visualizza_richieste_personali_dt(navigator_dt, df_dt)
+    st.divider()
 
 
 if __name__ == "__main__":
